7d86edb99d7a5188a9b0a1729c47a89159e5b714
moonrabbit1117 <moonrabbit1117@gmail.com>
2025-11-23 14:14:02 +0900

    first commit

---
+++ src/main/java/com/example/CoupangLite/CoupangLiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.CoupangLite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoupangLiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoupangLiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CoupangLite/CoupangLiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CoupangLite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoupangLiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
